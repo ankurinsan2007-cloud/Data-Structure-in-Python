4954f66b3dfa48c02bf0152b2caa0bbe27e7eae2
ANKUR <ankurinsan2007@gmail.com>
2026-03-28 15:50:11 +0530

    binary search

--- dsa.py
+++ dsa.py
@@ -7,3 +7,19 @@ class Solution:
                     return[i,j]
          
 
+"""binary search"""
+class Solution:
+    def search(self, nums, target):
+        l=0
+        h=len(nums)-1
+        while l<=h:
+            mid = (l+h)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid+1
+            else:
+                h = mid-1
+
+        return -1
+        
