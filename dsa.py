af8a3c9c60548b9a4023e508f309591bf6c44e25
ANKUR <ankurinsan2007@gmail.com>
2026-03-29 23:41:09 +0530

    3codes added

--- dsa.py
+++ dsa.py
@@ -1,4 +1,4 @@
-"""two sums"""
+"""two sums
 class Solution:
     def twoSum(self, nums, target: int):
         for i in range(len(nums)):
@@ -7,7 +7,7 @@ class Solution:
                     return[i,j]
          
 
-"""binary search"""
+binary search
 class Solution:
     def search(self, nums, target):
         l=0
@@ -22,4 +22,44 @@ class Solution:
                 h = mid-1
 
         return -1
+
+search insert position       
+class Solution:
+    def searchInsert(self, nums, target):
+        l=0
+        h=len(nums)-1
+        while l<=h:
+            mid = (l+h)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid+1
+            else:
+                h = mid-1
+
+        return l
+
+        move zeroes
+        class Solution:
+    def moveZeroes(self, nums: List[int]):
+        j=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                j+=1
+
+        return nums
+     
         
+
+        valid palindrome
+class Solution:
+    def isPalindrome(self, s: str):
+        s=s.lower()
+        new_s=""
+        for i in s:
+            if i.isalnum():
+                new_s+=i
+        
+        rev=new_s[::-1]
+        return rev==new_s"""
